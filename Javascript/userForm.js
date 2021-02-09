e8ec6c2a6af6f9e497470f6ef144497b97b2e821
sucheta-codekul <sucheta.codekul@gmail.com>
2021-02-09 11:29:41 +0530

    fetch

---
+++ Javascript/userForm.js
@@ -0,0 +1,37 @@
+function userValues() {
+  var userId = document.getElementById("userid").value;
+  var id = document.getElementById("id").value;
+  var title = document.getElementById("title").value;
+  if (document.getElementById("complete").checked) {
+    var completed = document.getElementById("complete").value;
+  } else {
+    var completed = (document.getElementById("complete").value = "No");
+  }
+
+  let userData = JSON.stringify({
+    userId: userId,
+    id: id,
+    title: title,
+    completed: completed,
+  });
+  //console.log(userData);
+  return userData;
+}
+
+function save() {
+  let user = userValues();
+  //   fetch("https://jsonplaceholder.typicode.com/posts", {
+  //     method: "POST",
+  //     body: user,
+  //     headers: { "Content-Type": "application/json" },
+  //   }).then((response) => {
+  //     console.log(response.json());
+  //   });
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  }).then((response) => {
+    console.log(response.json());
+  });
+}
