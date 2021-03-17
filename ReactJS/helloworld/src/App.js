331965ab4dc97006349561f1700705687badc090
chaudharisucheta <chaudharisucheta@gmail.com>
2021-03-17 12:04:41 +0530

    function component

--- ReactJS/helloworld/src/App.js
+++ ReactJS/helloworld/src/App.js
@@ -2,19 +2,30 @@ import logo from "./logo.svg";
 import "./App.css";
 import NewComponent from "./NewComponent";
 import DisplayComponent from "./DisplayComponent";
-const App = () => {
+import Employee from "./Employee";
+import TableComponent from "./TableComponent";
+import Clock from "./Clock";
+import FunComponent from "./FunComponent";
+function App() {
   let a = 10;
   let b = 20;
   let total = a + b;
+  let numbers = [100, 40, "Hello"];
+  let person = { firstName: "JOhn", city: "Pune" };
+  let employee = { salary: 40000, empid: 100 };
   return (
     <div>
       <h1>React Application</h1>
       <p>The total is: {total}</p>
       <DisplayComponent />
       <NewComponent />
+      <Employee person={person} employee={employee} numbers={numbers} />
+      <TableComponent />
+      <Clock message="Hello Its" />
+      <FunComponent employee={employee} lastName="XYZ" />
     </div>
   );
-};
+}
 //JSX
 
 export default App;
